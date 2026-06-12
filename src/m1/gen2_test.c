4870e5367c282ff1fd57efd4c8b3afc0d7cf5151
Rayyan Khan <rakrayyan16@gmail.com>
2026-06-12 12:26:49 +0530

    Phase 4: triage src/m1/ - move _inj_*.m1 to tests/injection/, move scattered tests, delete generated _test_*.c golden files

--- src/m1/gen2_test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-
-
-int64_t m0_read_file;
-
--- src/m1/m1c_test_output.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-
-
-int64_t m0_print(char*);
-
-int main(void) {
-  return m0_print("Hello, World\n");
-}
